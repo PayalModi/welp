0c8e5f1aa49372068b1201c342b0f487e0c6f944
JessicaPancer <jessica.pancer@gmail.com>
2016-12-01 16:27:44 -0500

    Update query_db.php

--- query_db.php
+++ query_db.php
@@ -1,32 +1,32 @@
 <html>
 <body>
 
-Cuisine is <?php echo $_GET["cuisine"]; ?><br>
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "finalproject";
-	$dbname = "cs437db";
+        $servername = "localhost";
+        $username = "root";
+        $password = "finalproject";
+        $dbname = "cs437db";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-	$cuisine = $_GET["cuisine"];
-	$sql = "SELECT name FROM restaurant WHERE cuisine = $cuisine";
-	$result = $conn->query($sql);
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $cuisine = $_GET["cuisine"];
+        $sql = "SELECT name FROM restaurant WHERE cuisine='$cuisine'";
+        $result = $conn->query($sql);
 
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        echo $row["name"];
-	    }
-	} else {
-	    echo "0 results";
-	}
-	$conn->close();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo $row["name"];
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
 ?>
+
 
 </html>
