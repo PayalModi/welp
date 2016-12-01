346c0bbe69978ae61bab5c055ab6cde3e199a00c
PayalModi <payalmodi14@gmail.com>
2016-12-01 16:46:29 -0500

    Update query_db.php

--- query_db.php
+++ query_db.php
@@ -1,32 +1,34 @@
 <html>
 <body>
 
-<?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "finalproject";
-        $dbname = "cs437db";
+<center>
+ 	<h1>Welp</h1>
+ 	<p>Welp is a tool to help you find blah.</p><br>
+	<?php
+	        $servername = "localhost";
+	        $username = "root";
+	        $password = "finalproject";
+	        $dbname = "cs437db";
+	        // Create connection
+	        $conn = new mysqli($servername, $username, $password, $dbname);
+	        // Check connection
+	        if ($conn->connect_error) {
+	            die("Connection failed: " . $conn->connect_error);
+	        }
+	        $ingredient = $_GET["ingredient"];
+	        $cuisine = $_GET["cuisine"];
+	        $price = $_GET["price"];
+	        $sql = "SELECT * FROM restaurant WHERE cuisine LIKE '%$cuisine' AND price LIKE '%$price'";
+	        $result = $conn->query($sql);
+	        if ($result->num_rows > 0) {
+	        	$random = rand(0, $result->num_rows-1);
+	        	$row = mysqli_data_seek($result, $random);
+	        } else {
+	            echo "0 results";
+	        }
+	        $conn->close();
+	?>
+</center>
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $cuisine = $_GET["cuisine"];
-        $sql = "SELECT name FROM restaurant WHERE cuisine LIKE '%$cuisine' AND price LIKE '%$price'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo $row["name"];
-            }
-        } else {
-            echo "0 results";
-        }
-        $conn->close();
-?>
-
-
+</body>
 </html>
