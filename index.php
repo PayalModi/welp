cdbc232e802cb5ca5ac37cd1ca026c935048dc18
PayalModi <payalmodi14@gmail.com>
2016-12-01 15:11:18 -0500

    Add sql query

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ input[type=text] {
     font-size: 16px;
     background-color: white;
     /*background-image: url('searchicon.png');*/
-    background-position: 10px 10px; 
-    background-repeat: no-repeat;
+    /*background-position: 10px 10px; */
+    /*background-repeat: no-repeat;*/
     padding: 12px 20px 12px 40px;
 }
 
@@ -26,6 +26,12 @@ input[type=text]:focus {
     width: 60%;
 }
 </style>
+
+<script>
+function searchForRestaurant() {
+	
+}
+</script>
 
 <head>
 	<title>Final Project</title>
@@ -45,6 +51,11 @@ input[type=text]:focus {
   			<option value="American">American</option>
   			<option value="French">French</option>
   			<option value="Mexican">Mexican</option>
+  			<option value="Turkish">Mexican</option>
+  			<option value="Tapas">Mexican</option>
+  			<option value="Asian">Mexican</option>
+  			<option value="Southwest">Southwest</option>
+  			<option value="Ethiopian">Ethiopian</option>
 		</select>
  		<select>
  			<option value="none">Price</option>
@@ -52,7 +63,35 @@ input[type=text]:focus {
   			<option value="$$">$$</option>
   			<option value="$$$">$$$</option>  			
 		</select>
+		<button type="button" onclick="searchForRestaurant()">Submit</button>
 	</center>
+
+	<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "finalproject";
+		$dbname = "cs437db";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "SELECT name FROM restaurant";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        echo $row["name"];
+		    }
+		} else {
+		    echo "0 results";
+		}
+		$conn->close();
+	?>
 </body>
 
 </html>
