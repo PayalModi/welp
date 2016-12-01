b5a001256285c5c5bf8fcb9aefba0136943a1044
PayalModi <payalmodi14@gmail.com>
2016-12-01 16:00:46 -0500

    Process query in separate file

---
+++ query_db.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+
+Cuisine is <?php echo $_GET["cuisine"]; ?><br>
+
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "finalproject";
+	$dbname = "cs437db";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	$sql = "SELECT name FROM restaurant WHERE cuisine = $_GET["cuisine"]";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo $row["name"];
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
+?>
+
+</body>
+</html>
