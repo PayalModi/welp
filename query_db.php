8fc070c6e0d86693b4b15ac7a60f54202c6eb951
PayalModi <payalmodi14@gmail.com>
2016-12-01 17:10:09 -0500

    Update query_db.php

--- query_db.php
+++ query_db.php
@@ -18,13 +18,16 @@
 	        $ingredient = $_GET["ingredient"];
 	        $cuisine = $_GET["cuisine"];
 	        $price = $_GET["price"];
-	        $sql = "SELECT * FROM restaurant, menu, item WHERE cuisine LIKE '%$cuisine' AND price LIKE '%$price' AND ingredient_List LIKE '%$ingredient%' AND item.item_id = menu.item_id AND menu.rest_id = restaurant.ID;";
+	        if ($price == '') {
+	        	$price = '%'
+	        }
+	        $sql = "SELECT * FROM restaurant, menu, item WHERE cuisine LIKE '%$cuisine' AND price LIKE '$price' AND ingredient_List LIKE '%$ingredient%' AND item.item_id = menu.item_id AND menu.rest_id = restaurant.ID;";
 	        $result = $conn->query($sql);
 	        if ($result->num_rows > 0) {
 	        	$random = rand(0, $result->num_rows-1);
 	        	mysqli_data_seek($result, $random);
 	        	$row = mysqli_fetch_row($result);
-	        	echo $row[0] ."\t". $row[2] ."\t". $row[3] ."\t".$row[4] ."\t". $row[5] ."\t". $row[10];
+	        	echo $row[0] ."\t". $row[2] ."\t". $row[3] ."\t".$row[4] ."\t". $row[5] ."\t". $row[8];
 	        } else {
 	            echo "0 results";
 	        }
