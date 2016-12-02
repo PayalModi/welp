e4ff5bca34a4d3b78b77ed963dce47f7d1f1cac6
JessicaPancer <jessica.pancer@gmail.com>
2016-12-01 23:52:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -78,6 +78,14 @@ input[type=text]:focus {
 			<hr width="33%">
 			<p id="filtersHeaders">Add a comment to a restaurant:</p>
 			<input type="text" name="username" placeholder="Username"><br>
+			<select name="price">
+	 			<option value="none">Rating</option>
+	  			<option value="1">1</option>
+	  			<option value="2">2</option>
+	  			<option value="3">3</option>  
+				<option value="4">3</option> 
+				<option value="5">3</option> 
+			</select>
 			<input type="text" name="restaurantcomm" placeholder="Restaurant"><br>
 			<input type="text" name="comment" placeholder="Comment"><br>
 			<input type="submit" value="Submit">
